8606546b5ee29dd43114791f5636dacebf12ff3e
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-09-07 18:30:10 +0200

    Support for numbers up to one million

--- src/LtNumbers.php
+++ src/LtNumbers.php
@@ -2,7 +2,7 @@
 namespace LtWords\LtNumbers;
  
 class LtNumbers {
-  private $basicNumbers = array(
+  private $_basicNumbers = array(
       "0" => "nulis",
       "1" => "vienas",
       "2" => "du",
@@ -24,6 +24,17 @@ class LtNumbers {
       "18" => "aštuoniolika",
       "19" => "devyniolika"
   );
+  
+  private $_tens = array(
+      "2" => "dvidešimt",
+      "3" => "trisdešimt",
+      "4" => "keturiasdešimt",
+      "5" => "penkiasdešimt",
+      "6" => "šešiasdešimt",
+      "7" => "septyniasdešimt",
+      "8" => "aštuoniasdešimt",
+      "9" => "devyniasdešimt",
+  );
  
   public function hasCheese($bool = true)
   {
@@ -37,9 +48,59 @@ class LtNumbers {
 	  }
       
       if ($number < 20) {
-		  return $this->basicNumbers[0 + $number];
+		  return $this->_basicNumbers[0 + $number];
 	  }
 
+      if ($number < 100) {
+		  $tens = floor($number / 10);
+		  $units = $number % 10;
+		  
+		  if ($units == 0) {
+			  return $this->_tens[0 + $tens];
+		  }
+		  
+		  return $this->_tens[0 + $tens] . " " . $this->_basicNumbers[0 + $units];
+	  }
+	  
+	  if ($number < 1000) {
+		  $hundreds = floor($number / 100);
+		  $tensUnits = $number % 100;
+		  
+		  if ($tensUnits == 0) {
+			  if ($hundreds == 1) {
+				  return "šimtas";
+			  }
+			  return $this->_basicNumbers[0 + $hundreds] . " šimtai";
+		  }
+		  
+		  if ($hundreds == 1) {
+			  return "šimtas " . $this->numberToText($tensUnits);
+		  }
+		  
+		  return $this->_basicNumbers[0 + $hundreds] . " šimtai " . $this->numberToText($tensUnits);
+	  }
+
+      if ($number < 1000000) {
+		  $thousands = floor($number / 1000);
+		  $hundredsTensUnits = $number % 1000;
+		  
+		  $thousandsWord = "tūkstančiai";
+		  if (($thousands % 10) == 1) {
+			  $thousandsWord = "tūkstantis";
+		  }
+		  if ($hundredsTensUnits == 0) {
+			  if ($thousands == 1) {
+				  return "tūkstantis";
+			  }
+			  return $this->numberToText($thousands) . " $thousandsWord";
+		  }
+		  
+		  if ($thousands == 1) {
+			  return "tūkstantis " . $this->numberToText($hundredsTensUnits);
+		  }
+		  
+		  return $this->numberToText($thousands) . " $thousandsWord " . $this->numberToText($hundredsTensUnits);
+	  }
 	  return "!";
   }
 }
--- tests/LtNumbersTest.php
+++ tests/LtNumbersTest.php
@@ -4,47 +4,83 @@ use LtWords\LtNumbers\LtNumbers;
  
 class LtNumbersTest extends PHPUnit_Framework_TestCase {
  
+  private $_ltNumbers;
+  
+  public function setUp()
+  {
+	  $this->_ltNumbers = new LtNumbers;
+  }
+  
   public function testHasCheese()
   {
-    $ltNumbers = new LtNumbers;
-    $this->assertTrue($ltNumbers->hasCheese());
+    $this->assertTrue($this->_ltNumbers->hasCheese());
   }
  
   public function testNumberToText()
   {
-	  $ltNumbers = new LtNumbers;
-	  
-	  $this->assertEquals("", $ltNumbers->numberToText(""));
-	  $this->assertEquals("", $ltNumbers->numberToText("fjfjeij"));
-	  $this->assertEquals("", $ltNumbers->numberToText("-------"));
-	  $this->assertNotEquals("", $ltNumbers->numberToText("0"));
-	  $this->assertNotEquals("", $ltNumbers->numberToText("0000"));
+	  $this->assertEquals("", $this->_ltNumbers->numberToText(""));
+	  $this->assertEquals("", $this->_ltNumbers->numberToText("fjfjeij"));
+	  $this->assertEquals("", $this->_ltNumbers->numberToText("-------"));
+	  $this->assertNotEquals("", $this->_ltNumbers->numberToText("0"));
+	  $this->assertNotEquals("", $this->_ltNumbers->numberToText("0000"));
   }
   
   public function testBasicNumbers()
   {
-	  $ltNumbers = new LtNumbers;
-	  
-	  $this->assertEquals("nulis", $ltNumbers->numberToText("0"));
-	  $this->assertEquals("vienas", $ltNumbers->numberToText("1"));
-	  $this->assertEquals("du", $ltNumbers->numberToText("2"));
-	  $this->assertEquals("trys", $ltNumbers->numberToText("3"));
-	  $this->assertEquals("keturi", $ltNumbers->numberToText("4"));
-	  $this->assertEquals("penki", $ltNumbers->numberToText("5"));
-	  $this->assertEquals("šeši", $ltNumbers->numberToText("6"));
-	  $this->assertEquals("septyni", $ltNumbers->numberToText("7"));
-	  $this->assertEquals("aštuoni", $ltNumbers->numberToText("8"));
-	  $this->assertEquals("devyni", $ltNumbers->numberToText("9"));
-	  $this->assertEquals("dešimt", $ltNumbers->numberToText("10"));
-	  $this->assertEquals("vienuolika", $ltNumbers->numberToText("11"));
-	  $this->assertEquals("dvylika", $ltNumbers->numberToText("12"));
-	  $this->assertEquals("trylika", $ltNumbers->numberToText("13"));
-	  $this->assertEquals("keturiolika", $ltNumbers->numberToText("14"));
-	  $this->assertEquals("penkiolika", $ltNumbers->numberToText("15"));
-	  $this->assertEquals("šešiolika", $ltNumbers->numberToText("16"));
-	  $this->assertEquals("septyniolika", $ltNumbers->numberToText("17"));
-	  $this->assertEquals("aštuoniolika", $ltNumbers->numberToText("18"));
-	  $this->assertEquals("devyniolika", $ltNumbers->numberToText("19"));
+	  $this->assertEquals("nulis", $this->_ltNumbers->numberToText("0"));
+	  $this->assertEquals("vienas", $this->_ltNumbers->numberToText("1"));
+	  $this->assertEquals("du", $this->_ltNumbers->numberToText("2"));
+	  $this->assertEquals("trys", $this->_ltNumbers->numberToText("3"));
+	  $this->assertEquals("keturi", $this->_ltNumbers->numberToText("4"));
+	  $this->assertEquals("penki", $this->_ltNumbers->numberToText("5"));
+	  $this->assertEquals("šeši", $this->_ltNumbers->numberToText("6"));
+	  $this->assertEquals("septyni", $this->_ltNumbers->numberToText("7"));
+	  $this->assertEquals("aštuoni", $this->_ltNumbers->numberToText("8"));
+	  $this->assertEquals("devyni", $this->_ltNumbers->numberToText("9"));
+	  $this->assertEquals("dešimt", $this->_ltNumbers->numberToText("10"));
+	  $this->assertEquals("vienuolika", $this->_ltNumbers->numberToText("11"));
+	  $this->assertEquals("dvylika", $this->_ltNumbers->numberToText("12"));
+	  $this->assertEquals("trylika", $this->_ltNumbers->numberToText("13"));
+	  $this->assertEquals("keturiolika", $this->_ltNumbers->numberToText("14"));
+	  $this->assertEquals("penkiolika", $this->_ltNumbers->numberToText("15"));
+	  $this->assertEquals("šešiolika", $this->_ltNumbers->numberToText("16"));
+	  $this->assertEquals("septyniolika", $this->_ltNumbers->numberToText("17"));
+	  $this->assertEquals("aštuoniolika", $this->_ltNumbers->numberToText("18"));
+	  $this->assertEquals("devyniolika", $this->_ltNumbers->numberToText("19"));
+  }
+  
+  public function testBasicTens()
+  {
+	  $this->assertEquals("dvidešimt", $this->_ltNumbers->numberToText("20"));
+	  $this->assertEquals("trisdešimt", $this->_ltNumbers->numberToText("30"));
+  }
+  
+  public function testLessThan100()
+  {
+	  $this->assertEquals("dvidešimt vienas", $this->_ltNumbers->numberToText("21"));
+	  $this->assertEquals("trisdešimt du", $this->_ltNumbers->numberToText("32"));
+	  $this->assertEquals("devyniasdešimt aštuoni", $this->_ltNumbers->numberToText("98"));
+  }
+  
+  public function testLessThan1000()
+  {
+	  $this->assertEquals("šimtas", $this->_ltNumbers->numberToText("100"));
+	  $this->assertEquals("šimtas vienas", $this->_ltNumbers->numberToText("101"));
+	  $this->assertEquals("šimtas dešimt", $this->_ltNumbers->numberToText("110"));
+	  $this->assertEquals("šimtas dvidešimt", $this->_ltNumbers->numberToText("120"));
+	  $this->assertEquals("šimtas dvidešimt trys", $this->_ltNumbers->numberToText("123"));
+	  $this->assertEquals("šimtas septyniasdešimt trys", $this->_ltNumbers->numberToText("173"));
+	  $this->assertEquals("trys šimtai septyniasdešimt trys", $this->_ltNumbers->numberToText("373"));
+	  $this->assertEquals("aštuoni šimtai septyniasdešimt septyni", $this->_ltNumbers->numberToText("877"));
+  }
+  
+  public function testLessThanOneMillion()
+  {
+      $this->assertEquals("tūkstantis", $this->_ltNumbers->numberToText("1000"));
+      $this->assertEquals("du tūkstančiai", $this->_ltNumbers->numberToText("2000"));
+
+	  $this->assertEquals("aštuoni šimtai septyniasdešimt septyni tūkstančiai trys šimtai dvidešimt vienas", $this->_ltNumbers->numberToText("877321"));
+	  $this->assertEquals("aštuoni šimtai septyniasdešimt vienas tūkstantis trys šimtai dvidešimt vienas", $this->_ltNumbers->numberToText("871321"));
   }
 }
 
